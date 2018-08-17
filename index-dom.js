1d1da03c6ee02f035a2b42b0badee975ee7ab5f0
Nathri <nathrijacobs@gmail.com>
2018-08-17 12:41:26 +0200

    more random numbers

--- index-dom.js
+++ index-dom.js
@@ -1,8 +1,18 @@
 window.onload = random;
+var blocked =   document.querySelectorAll('.rand')
+
+
 
   function random() {
+    for(var i = 0 ; i<blocked.length ; i++){
+      var elems = blocked[i]
  var num = Math.floor(Math.random() * 9);
+     
+    showNum(elems, num)
+  }
+  }
 
-  document.querySelector('.rand').innerHTML = num
+function showNum(elems, num){
+elems.innerHTML = num
   
 }
